3a74a386e79130f130f822e2ba5a4c1dde6de092
Edy <drobeduard@gmail.com>
2025-05-07 18:52:50 +0300

    Add stars calculation

---
+++ open-table/app/components/Stars.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import fullStar from "../../public/icons/full-star.png";
+import halfStar from "../../public/icons/half-star.png";
+import emptyStar from "../../public/icons/empty-star.png";
+import Image from "next/image";
+import { Review } from '@/prisma/generated/prisma/client';
+import { calculateReviewRatingAverage } from '@/utilities/calculateReviewRatingAverage';
+
+export default function Stars({ reviews }: { reviews: Review[] }) {
+
+    const rating = calculateReviewRatingAverage(reviews);
+
+    const renderStars = () => {
+        const stars = [];
+
+        for (let i = 0; i < 5; i++) {
+            const diff = parseFloat((rating - i).toFixed(1));
+            if (diff >= 1) stars.push(fullStar);
+            else if (diff < 1 && diff > 0) {
+                if (diff <= 0.2) stars.push(emptyStar);
+                else if (diff > 0.2 && diff <= 0.6) stars.push(halfStar);
+                else stars.push(fullStar);
+            }
+            else stars.push(emptyStar);
+        }
+
+        return stars.map(star => {
+            return (
+                <Image src={star} alt="" className="w-4 h-4 mr-1" />
+            )
+        })
+    }
+
+    return <div className="flex items-center">{renderStars()}</div>
+}
