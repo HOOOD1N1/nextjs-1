18a4259a097a26dda68a16d1fb92671204e2bcc0
Edy <drobeduard@gmail.com>
2025-05-07 12:46:48 +0300

    Add price logic

--- open-table/app/page.tsx
+++ open-table/app/page.tsx
@@ -1,22 +1,49 @@
-
-import { Inter } from "next/font/google";
-
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient, Cuisine, Location, PRICE } from "../prisma/generated/prisma/client";
 
-const inter = Inter({
-  subsets: ['latin'],
-  weight: '400',
-})
 
-export default function Home() {
+const prisma = new PrismaClient();
 
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+}
+
+const fetchRestaurant = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+      slug: true
+    }
+  });
+
+  return restaurants;
+}
+
+export default async function Home() {
+
+  const restaurants = await fetchRestaurant();
+
+  console.log("restaurants ", restaurants)
   return (
-      <main>
-        <Header />
-        <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-          <RestaurantCard />
-        </div>
-      </main>
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {restaurants.map(restaurant => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
+      </div>
+    </main>
   );
 }
