8764fd02a82dfad707ae4e397780f819024bbd66
Edy <drobeduard@gmail.com>
2025-05-07 14:23:47 +0300

    Fetching data in restaurant page

--- open-table/app/restaurant/[slug]/page.tsx
+++ open-table/app/restaurant/[slug]/page.tsx
@@ -8,24 +8,61 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@/prisma/generated/prisma/client";
 
-export default function RestaurantDetails()
-{
-    return (
-      <>
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Title />
-            <Rating />
-            <Description />
-            
-            <Images />
-            <Reviews />
-            
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-      </>
-    );
+const prisma = new PrismaClient;
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    }
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+}
+
+export default async function RestaurantDetails(props: { params: { slug: string } }) {
+
+  const { slug } = await props.params;
+  console.log(slug);
+
+  const restaurant = await fetchRestaurantBySlug(slug);
+
+  return (
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
+        <Rating />
+        <Description description={restaurant.description} />
+
+        <Images images={restaurant.images} />
+        <Reviews />
+
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+    </>
+  );
 }
