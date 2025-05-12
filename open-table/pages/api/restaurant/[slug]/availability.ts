5c6d8d91470913cd422c4c1fc3c78318eb83a464
Edy <drobeduard@gmail.com>
2025-05-13 01:29:14 +0300

    add availability endpoint

---
+++ open-table/pages/api/restaurant/[slug]/availability.ts
@@ -0,0 +1,19 @@
+import { error } from "console";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { slug, day, time, partySize } = req.query as {
+        slug: string;
+        day: string;
+        time: string;
+        partySize: string;
+    };
+
+    if (!day || !time || !partySize) {
+        return res.status(400).json({
+            errorMessage: "Invalid data"
+        })
+    }
+
+    return res.json({ slug, day, time, partySize })
+}
