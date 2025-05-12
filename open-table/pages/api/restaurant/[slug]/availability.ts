466b6d8e587299f9a5dc10c339cb1644bde7f63e
Edy <drobeduard@gmail.com>
2025-05-13 01:36:54 +0300

    send search times

--- open-table/pages/api/restaurant/[slug]/availability.ts
+++ open-table/pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,6 @@
 import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
+import { times } from "../../../../data";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { slug, day, time, partySize } = req.query as {
@@ -15,5 +16,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         })
     }
 
-    return res.json({ slug, day, time, partySize })
+    const searchTimes = times.find(t => {
+        return t.time === time
+    })?.searchTimes;
+
+    if (!searchTimes) {
+        return res.status(400).json({
+            errorMessage: "Invalid data provided"
+        })
+    }
+
+    return res.json({ searchTimes })
 }
